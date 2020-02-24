c00f32424f897ced411d46809dc12d3232c74e95
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-25 01:07:10 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    
+    if(n>0){
+        return Number(n.toString().split('').reverse().join(''));
+    } else{
+        return Number(n.toString().split('').slice(1).reverse().join(''));
+    }
+    
 }
